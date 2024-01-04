2917d863874f23cdeab8727178cfe8844cefda52
shmuelko <shmuel777@gmail.com>
2024-01-04 16:13:08 +0200

    complete

--- resistor-color.ts
+++ resistor-color.ts
@@ -1,5 +1,5 @@
-export const colorCode = () => {
-  throw new Error('Delete this line and implement this function')
+export const colorCode = (color: string) => {
+  return COLORS.indexOf(color)
 }
 
-export const COLORS = undefined
+export const COLORS: string[] = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
